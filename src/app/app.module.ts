782ab814b7624415e0fb4a0aac9fa7e2bfcddc91
BrianJArmagost <BArmagost@DataRecognitionCorp.com>
2019-01-16 15:43:18 -0600

    guild001 - reall cool changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    Page1Component,
+    Page2Component
   ],
   imports: [
     BrowserModule,
